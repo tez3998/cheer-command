505652f69d7f248cf74f90912b86bb7eb273fba0
Yuta Tezuka <yuta.tzk@gmail.com>
2022-06-02 16:10:15 +0900

    Add more motion

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 int main() {
     char *frame_1 = "frames/normal";
     char *frame_2 = "frames/handsup";
+    char *frame_3 = "frames/handsup2";
 
     int num_line;
 
@@ -15,6 +16,14 @@ int main() {
         clear_frame(num_line);
 
         num_line = print_frame(frame_2);
+        sleep(1);
+        clear_frame(num_line);
+
+        num_line = print_frame(frame_1);
+        sleep(1);
+        clear_frame(num_line);
+
+        num_line = print_frame(frame_3);
         sleep(1);
         clear_frame(num_line);
     }
