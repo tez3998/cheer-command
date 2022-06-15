8bd1751cc01e37c4f46a95b99164b8f73dc2e5cf
Yuta Tezuka <yuta.tzk@gmail.com>
2022-06-16 05:43:16 +0900

    Refactor code

--- src/main.c
+++ src/main.c
@@ -3,29 +3,27 @@
 #include "print_frame.h"
 #include "clear_frame.h"
 
+void print_and_clear_frame(char *frame_path) {
+    int SLEEP_SEC = 1;
+    int num_line;
+
+    num_line = print_frame(frame_path);
+    sleep(SLEEP_SEC);
+    clear_frame(num_line);
+}
+
+
 int main() {
     char *frame_1 = "../frames/normal";
     char *frame_2 = "../frames/handsup";
     char *frame_3 = "../frames/handsup2";
-
     int num_line;
 
     while (1) {
-        num_line = print_frame(frame_1);
-        sleep(1);
-        clear_frame(num_line);
-
-        num_line = print_frame(frame_2);
-        sleep(1);
-        clear_frame(num_line);
-
-        num_line = print_frame(frame_1);
-        sleep(1);
-        clear_frame(num_line);
-
-        num_line = print_frame(frame_3);
-        sleep(1);
-        clear_frame(num_line);
+        print_and_clear_frame(frame_1);
+        print_and_clear_frame(frame_2);
+        print_and_clear_frame(frame_1);
+        print_and_clear_frame(frame_3);
     }
 
     return 0;
