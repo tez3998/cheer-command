a73ff7858297aa9e0c65d8728652b1068396c125
Yuta Tezuka <yuta.tzk@gmail.com>
2022-06-24 20:38:18 +0900

    Remove unnecessary code

--- src/main.c
+++ src/main.c
@@ -17,7 +17,6 @@ int main() {
     char *frame_1 = "../frames/normal";
     char *frame_2 = "../frames/handsup";
     char *frame_3 = "../frames/handsup2";
-    int num_line;
 
     while (1) {
         print_and_clear_frame(frame_1);
